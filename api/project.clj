a2714398911698348ad985b81839e5c0e582ba33
David Soria Parra <dsp@php.net>
2011-04-18 01:36:10 +0200

    API Version bump to 1.1.0

--- api/project.clj
+++ api/project.clj
@@ -1,4 +1,4 @@
-(defproject geocommit "1.0.0-SNAPSHOT"
+(defproject geocommit "1.1.0"
   :description "geocommit.com HTTP API"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
