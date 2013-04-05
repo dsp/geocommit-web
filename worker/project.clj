d7e7667064b85523bc99ec8b74d90cac1f5fb08a
Nils Adermann <naderman@naderman.de>
2013-04-05 15:27:44 +0200

    geocommit-web is MIT licensed

--- worker/project.clj
+++ worker/project.clj
@@ -1,7 +1,7 @@
 (defproject worker "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+  :description "Geocommit background worker"
+  :url "http://github.com/dsp/geocommit-web/"
+  :license {:name "MIT"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"],
                  [com.novemberain/langohr "1.0.0-beta13"],
